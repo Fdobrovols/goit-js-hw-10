085ae203863d5e9b0d8a3796304dfd56ea30df1f
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-03-23 23:33:22 +0200

    New_Updates

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,13 @@
 const url = new URLSearchParams({});
 
-export function fetchCountries(name) {
+export function fetchCountries(name) 
+{
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => 
+    ).then(response => 
     {
     if (!response.ok)
       throw new Error(`Oops, there is no country with that name`);
     return response.json();
   });
-  
 }
--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const divEl = document.querySelector('.country-info');
 
 inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-function onSearch(e) {
+function onSearch(e) 
+{
   const countryName = e.target.value.trim().toLowerCase();
 
   if (countryName === '') {
@@ -20,52 +21,53 @@ function onSearch(e) {
 
   fetchCountries(countryName)
     .then(renderMarkup)
-    .catch(error => {
+    .catch(error => 
+    {
       Notiflix.Notify.failure(error.message);
       insertHtml();
     });
 }
 
-function renderMarkup(data) {
+function renderMarkup(data) 
+{
   let markup = null;
-  if (data.length >= 2 && data.length <= 10) {
-    markup = data
-      .map(({ name, flags }) => {
-        return `<li class="country__item"><img src="${flags.svg}" alt="${flags.alt}" class="country__img" /><h2 class="country__heading">${name.official}</h2>
-        </li>`;
-      })
-      .join('');
-    insertHtml(markup);
+  if (data.length >= 2 && data.length <= 10) 
+  {
+       markup = data
+      .map(({ name, flags }) => 
+      {
+        return `<li class="country__item"><img src="${flags.svg}" alt="${flags.alt}" class="country__img" /><h2 class="country__heading">
+        ${name.official}</h2></li>`;
+      }).join('');
+      insertHtml(markup);
     return;
   }
-  if (data.length > 10) {
-    Notiflix.Notify.info(
-      'Too many matches found. Please enter a more specific name.'
-    );
+  if (data.length > 10) 
+  {
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
     return;
   }
 
   const { name, capital, population, flags, languages } = data[0];
   const allLanguages = Object.entries(languages)
-    .map(item => item[1])
-    .join(', ');
-  markup = `
-    <div class="country-info__wrapper"><img src="${flags.svg}" alt="${
-    flags.alt
-  }" class="country-info__img" />
+    .map(item => item[1]).join(', ');
+    markup = `
+    <div class="country-info__wrapper">
+    <img src="${flags.svg}" alt="${flags.alt}" class="country-info__img" />
         <h2 class="country-info__heading">${name.official}</h2></div>
         <ul class="country-info__list">
+
         <li class="country-info__item"><span class="country-info__name">Capital: </span>${capital}</li>
         <li class="country-info__item"><span class="country-info__name">Population: </span>${population}</li>
-        <li class="country-info__item"><span class="country-info__name">Language${
-          Object.keys(languages).length === 1 ? '' : 's'
-        }: </span>${allLanguages}</li>
-        </ul>
-      `;
-  insertHtml('', markup);
+        <li class="country-info__item"><span class="country-info__name">Language${Object.keys(languages).length === 1 ? '' : 's'}: 
+        </span>${allLanguages}</li>
+
+        </ul>`;
+       insertHtml('', markup);
 }
 
-function insertHtml(intoUl = '', intoDiv = '') {
+function insertHtml(intoUl = '', intoDiv = '') 
+{
   listEl.innerHTML = intoUl;
   divEl.innerHTML = intoDiv;
 }
